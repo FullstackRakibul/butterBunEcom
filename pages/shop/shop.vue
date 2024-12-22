e06f7f10b2c7556d12db9581567d8442b8f064b5
FullstackRakibul <95490833+FullstackRakibul@users.noreply.github.com>
2024-12-22 13:49:51 +0600

    initial commit

---
+++ pages/shop/shop.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <template>
+    <h1 class="text-3xl font-bold underline">
+      SHOP PAGE !!!
+    </h1>
+  </template>
+</template>
